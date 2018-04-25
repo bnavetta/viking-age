940507880750dcc88b09ec620ce4e42bf1e69e59
Ben Navetta <ben.navetta@gmail.com>
2018-04-25 03:03:16 -0400

    trade more rapidly and shuffle resources

--- core/src/main/java/com/bennavetta/vikings/engine/systems/TradeSystem.java
+++ core/src/main/java/com/bennavetta/vikings/engine/systems/TradeSystem.java
@@ -54,6 +54,7 @@ public class TradeSystem extends IntervalIteratingSystem
 
                 ResourcesComponent.ResourceHandle[] resources = new ResourcesComponent.ResourceHandle[ResourcesComponent.ALL_RESOURCES.length];
                 System.arraycopy(ResourcesComponent.ALL_RESOURCES, 0, resources, 0, resources.length);
+                shuffle(resources);
 
                 for (ResourcesComponent.ResourceHandle ourRes : resources)
                 {
@@ -67,10 +68,29 @@ public class TradeSystem extends IntervalIteratingSystem
                                 && ourTrade.value.get(theirRes, 0f) > theirTrade.value.get(theirRes, 0f);
                         if (shouldTrade && ourRes.get(ourResources) >= 10 && theirRes.get(theirResources) >= 10)
                         {
-                            ourRes.increment(ourResources, -10);
-                            theirRes.increment(ourResources, 10);
-                            theirRes.increment(theirResources, -10);
-                            ourRes.increment(theirResources, 10);
+                            float amount;
+                            float limiter = Math.min(ourRes.get(ourResources), theirRes.get(theirResources));
+                            if (limiter >= 1000)
+                            {
+                                amount = 100;
+                            }
+                            else if (limiter >= 500)
+                            {
+                                amount = 50;
+                            }
+                            else if (limiter >= 100)
+                            {
+                                amount = 10;
+                            }
+                            else
+                            {
+                                amount = 1;
+                            }
+
+                            ourRes.increment(ourResources, -amount);
+                            theirRes.increment(ourResources, amount);
+                            theirRes.increment(theirResources, -amount);
+                            ourRes.increment(theirResources, amount);
 
                             return; // Only trade one unit per iteration
                         }
@@ -82,6 +102,20 @@ public class TradeSystem extends IntervalIteratingSystem
                 float newGoodwill = Math.min(ourGoodwill * 1.5f, 1f);
                 ourTrade.goodwill.put(partner, newGoodwill);
             }
+        }
+    }
+
+    /**
+     * Fisher-Yates Shuffle
+     */
+    private static <T> void shuffle(T[] arr)
+    {
+        for (int i = arr.length - 1; i > 0; i--)
+        {
+            int j = MathUtils.random(i);
+            T tmp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = tmp;
         }
     }
 }
